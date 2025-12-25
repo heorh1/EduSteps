3f80c75fca7426936603f82a1214c6e6ab5b20b9
heorh1 <heorhi44@gmail.com>
2025-12-25 19:32:54 +0100

    initial commit

---
+++ EduSteps/EduSteps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EduSteps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
